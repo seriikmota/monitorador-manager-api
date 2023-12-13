1337ac5d0faae33cea1fc85f5c182f4299b75b55
Erik Mota <seriikmota.dev@gmail.com>
2023-12-13 10:52:19 -0300

    Iniciando projeto

---
+++ src/main/java/dev/erikmota/desafiounikamain/DesafioUnikaMainApplication.java
@@ -0,0 +1,13 @@
+package dev.erikmota.desafiounikamain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioUnikaMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioUnikaMainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/erikmota/desafiounikamain/DesafioUnikaMainApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.erikmota.desafiounikamain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioUnikaMainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
